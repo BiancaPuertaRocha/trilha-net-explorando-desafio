190f39c5342fb55dcdcb6dad19395dc32a933a6f
BiancaPuertaRocha <biapuertarocha@gmail.com>
2022-10-11 10:49:15 -0300

    solving challenge

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,20 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (this.Suite != null && this.Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                if(this.Suite == null)
+                {
+                    throw new Exception("atributo Suite nulo");
+                }
+                else
+                {
+                    throw new Exception("a capacidade da suite eh menor que o numero de hospedes");
+                }
             }
         }
 
@@ -35,23 +39,27 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            if(this.Hospedes == null)
+            {
+                throw new Exception("atributo Hospedes nulo");
+            }
+            else
+            {
+                return this.Hospedes.Count;
+            }
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
-
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if(this.Suite == null || this.DiasReservados == 0)
             {
-                valor = 0;
+                throw new Exception("Suite e/ou DiasReservados invalidos");
+            }
+            decimal valor = this.Suite.ValorDiaria * this.DiasReservados;
+
+            if (this.DiasReservados >= 10)
+            {
+                valor = valor * 0.9m;
             }
 
             return valor;
